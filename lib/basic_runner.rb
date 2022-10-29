a9bd126320e3c33a71818a254f067d1d4b08f874
Patrick Wendo <wendonyang@gmail.com>
2022-10-29 09:40:06 +0300

    feat(main)
    - added in the ability to test a blocks equality to a boolean value

--- lib/basic_runner.rb
+++ lib/basic_runner.rb
@@ -1,3 +1,25 @@
 class Runner
+  attr_accessor :test_space
   def initialize; end
+
+  def expects
+    yield
+  end
+
+  def to_be(*args, bool_value)
+    if [TrueClass, FalseClass].include?(bool_value.class)
+      block = yield
+      return block == bool_value
+    else
+      raise ArgumentError, "Expected a boolean, got #{bool_value.class}"
+    end
+  end
+
+  def to_eq
+
+  end
+
+  def it(string)
+    yield
+  end
 end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -9,7 +9,23 @@ RSpec.describe Runner do
     end
   end
 
-  describe "Runner#test" do
-    it "accepts "
+  describe "Runner#accept" do
+    let(:runner){Runner.new}
+    it "accepts a string and runs whatever block is passed to it" do
+      expect(runner.expects{ true }).to be true
+    end
+  end
+
+  describe "Runner#to_be" do
+    let(:runner){Runner.new}
+
+    it "fails if a boolean value is not provided" do
+      expect{ runner.to_be("string"){true} }.to raise_error ArgumentError
+    end
+
+    it "accepts a string and a boolean and compares against the boolean" do
+      expect(runner.to_be(true){true }).to be true
+    end
+
   end
 end
