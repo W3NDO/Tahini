c75c54a2a9220fc9bac43de2ee4fc8565ad1cd74
W3NDO <wendonyang@gmail.com>
2022-11-02 20:40:50 +0300

    feat(main)
    - Built a custom matcher for the result class(Pass/Fail). Confirms that there is a match on class type, lhs & rhs values and in the case of a Failure, it also confirms the error message.

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -21,8 +21,8 @@ RSpec.describe Runner do
     let(:runner){Runner.new}
 
     it "accepts a string and a boolean and compares against the boolean" do
-      expect(runner.to_be(true){ false }).to be_instance_of Fail
-      expect(runner.to_be(true){ true }).to be_instance_of Pass
+      expect(runner.to_eq(true){ 1 == 1 }).to have_result_with_lhs_rhs(Pass.new(true, true))
+      expect(runner.to_be(false){ 35 < 70 }).to have_result_with_lhs_rhs(Fail.new(true, false))
     end
   end
 
@@ -30,14 +30,14 @@ RSpec.describe Runner do
     let(:runner){Runner.new}
 
     it "returns a fail if the classes of the expected and actual do not match" do
-      expect(runner.to_eq([1,2,3]){"1,2,3"}).to be_instance_of Fail
+      expect(runner.to_eq([1,2,3]){"1,2,3"}).to have_result_with_lhs_rhs(Fail.new("1,2,3", [1,2,3], message: "Class mismatch between the expected value: '[1, 2, 3]', and the actual value '1,2,3'"))
     end
 
     it "correctly compares strings, booleans, numbers" do
-      expect(runner.to_eq("Hello world"){"Hello world"}).to be_instance_of Pass
-      expect(runner.to_eq(1){1}).to be_instance_of  Pass
-      expect(runner.to_eq(true){true}).to be_instance_of Pass
-      expect(runner.to_eq(false){false}).to be_instance_of Pass
+      expect(runner.to_eq("hello"){"hello"}).to have_result_with_lhs_rhs(Pass.new("hello", "hello"))
+      expect(runner.to_eq(1){1}).to have_result_with_lhs_rhs(Pass.new(1, 1))
+      expect(runner.to_eq(true){true}).to have_result_with_lhs_rhs(Pass.new(true, true))
+      expect(runner.to_eq(false){false}).to have_result_with_lhs_rhs(Pass.new(false, false))
     end
   end
 
@@ -45,18 +45,17 @@ RSpec.describe Runner do
     let(:runner){ Runner.new }
 
     it "correctly compares collections" do
-      expect(runner.collections_to_eq([1,2,3]){[1,2,3]}).to be_instance_of Pass
-      expect(runner.collections_to_eq({:hello => "world"}){ {:hello => "world"} } ).to be_instance_of Pass
+      expect(runner.collections_to_eq([1,2,3]){[1,2,3]}).to have_result_with_lhs_rhs(Pass.new([1,2,3], [1,2,3]))
+      expect(runner.collections_to_eq({:hello => "world"}){ {:hello => "world"} }).to have_result_with_lhs_rhs(Pass.new({:hello => "world"}, {:hello => "world"}))
     end
 
     it "fails if a non-collection object is passed as the expected value" do
-      expect(runner.collections_to_eq("string"){[1,2,3]}).to be_instance_of Fail
-      expect(runner.collections_to_eq("string"){[1,2,3]}.message).to eq("#collections_to_eq only compares Array or Hash objects. 'string' is not a valid collection at this time")
+      expect(runner.collections_to_eq("string"){[1,2,3]}).to have_result_with_lhs_rhs(Fail.new([1,2,3], "string", message: "#collections_to_eq only compares Array or Hash objects. 'string' is not a valid collection at this time" ))
+
     end
 
     it "fails if a non-collection object is returned from the execution as the actual value" do
-      expect(runner.collections_to_eq([1,2,3]){"string"}).to be_instance_of Fail
-      expect(runner.collections_to_eq([1,2,3]){"string"}.message).to eq("#collections_to_eq only compares Array or Hash objects. The evaluated result, 'string', is not a valid collection at this time.")
+      expect(runner.collections_to_eq([1,2,3]){"string"}).to have_result_with_lhs_rhs(Fail.new("string", [1,2,3], message: "#collections_to_eq only compares Array or Hash objects. The evaluated result, 'string', is not a valid collection at this time." ))
     end
   end
 end
